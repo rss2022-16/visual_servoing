38a35748e2caa3828bcf1c1e5ac6e4ecabfa3583
PaperCloud <haoni.amy@gmail.com>
2022-03-07 20:22:07 +0000

    added color segmentation code

--- scripts/computer_vision/color_segmentation.py
+++ scripts/computer_vision/color_segmentation.py
@@ -34,10 +34,33 @@ def cd_color_segmentation(img, template):
 				(x1, y1) is the top left of the bbox and (x2, y2) is the bottom right of the bbox
 	"""
 	########## YOUR CODE STARTS HERE ##########
-
-	bounding_box = ((0,0),(0,0))
+	hsv_image = cv2.cvtColor(img,cv2.COLOR_BGR2HSV)
+	# image_print(hsv_image)
+	orange_min = np.array([8, 50, 50])
+	orange_max = np.array([15,255, 255])
+	mask = cv2.inRange(hsv_image, orange_min, orange_max)
+	# image_print(mask)
+	erosion_kernel = np.ones((15,15), np.uint8)
+	img_erosion = cv2.erode(mask, erosion_kernel, iterations=1)  
+	# image_print(img_erosion)
+	dilation_kernel = np.ones((20,20), np.uint8)
+	img_dilation = cv2.dilate(img_erosion, dilation_kernel, iterations=1)   
+	# image_print(img_dilation)
+	segmented_img = cv2.bitwise_and(img, img, mask=img_dilation)
+	# image_print(segmented_img)
+	contours= cv2.findContours(img_dilation.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+	
+	x,y,w,h = cv2.boundingRect(contours[0])
+	out = cv2.rectangle(img, (x,y), (x+w, y+h),(0,255,0),2)
+	image_print(out)
+	bounding_box = ((x,y),(x+w,y+h))
 
 	########### YOUR CODE ENDS HERE ###########
 
 	# Return bounding box
 	return bounding_box
+
+if __name__ == "__main__":
+	img = cv2.imread('./test_images_cone/test14.jpg')
+	cd_color_segmentation(img, None)
+
